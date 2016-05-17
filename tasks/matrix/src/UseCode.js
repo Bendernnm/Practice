33b71c8c14988838a6ebe14c052a457a6ed6feb1
Bender <bender.d1mon@gmail.com>
2016-05-17 18:29:26 +0300

    add some menu change.

--- tasks/matrix/src/UseCode.js
+++ tasks/matrix/src/UseCode.js
@@ -1,10 +1,8 @@
-//var obj1 = new WordMatrix("test1");
-//obj1.init();
+var obj1 = new WordMatrix("test1");
+obj1.init();
 
-//var obj2 = new IntegerMatrix("test2");
-//obj2.init();
+var obj2 = new IntegerMatrix("test2");
+obj2.init();
 
-var str = "-22.3.12";
-var regExp =  /^[+-]?\d+[.]?\d+$/;
-var check_result = check(str, regExp);
-alert(check_result);
+var obj3 = new DoubleMatrix("test3");
+obj3.init();
